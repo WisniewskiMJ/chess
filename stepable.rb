ba4589019015b72f24e7618e3a45036c0be1aa95
Bakshee <bakshee.jw@gmail.com>
2020-10-04 01:56:39 +0200

    Move part of Stepable module to Movable

--- stepable.rb
+++ stepable.rb
@@ -1,22 +1,13 @@
+require_relative "movable"
+
 module Stepable
+  include Movable
 
 def moves
-  elements = [:-,:+,move_diffs[0],move_diffs[1]]
-  elements += elements
-  mix = elements.permutation.to_a.uniq
-  permutations = [] 
-  mix.each do |p|
-    p_even = p.select.with_index {|el, i| i.even?}
-    permutations << p if !permutations.include?(p) && p_even.all? {|e| e.instance_of?(Symbol)} 
-  end
-  (0...permutations.length).each do |n|
-    permutations.push(permutations.shift[0..3])
-  end
-  permutations = permutations.uniq
-  stepable_permutations = permutations.select.with_index {|e| e[1] != e[3]}
+  directions = permutations.select.with_index {|e| e[1] != e[3]}
   moves = []
-  (0...stepable_permutations.length).each do |n|
-    moves << [@pos[0].send(stepable_permutations[n][0],stepable_permutations[n][1]),@pos[1].send(stepable_permutations[n][2],stepable_permutations[n][3])]
+  (0...directions.length).each do |n|
+    moves << [@pos[0].send(directions[n][0],directions[n][1]),@pos[1].send(directions[n][2],directions[n][3])]
   end
   moves
 end
