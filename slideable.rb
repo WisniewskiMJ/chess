d5c3eddb7ff194d814488691fc03a8658e63967f
Bakshee <bakshee.jw@gmail.com>
2020-10-05 18:37:14 +0200

    Add Slideable#stretch_moves

--- slideable.rb
+++ slideable.rb
@@ -1,4 +1,5 @@
 require_relative "movable"
+require "byebug"
 
 module Slideable
   include Movable
@@ -10,10 +11,26 @@ module Slideable
       new_pos = [@pos[0].send(directions[n][0],directions[n][1]),@pos[1].send(directions[n][2],directions[n][3])] 
       moves << new_pos if new_pos != @pos
     end
-    moves.uniq
+    moves = moves.uniq
+    stretch_moves(moves)
   end
 
   private
+
+  def stretch_moves(moves)
+    lines = []
+    steps = moves.map {|mov| [mov[0] - @pos[0], mov[1] - @pos[1]]}
+    moves.each_with_index do |dir, idx|
+      line = [moves[idx]]
+      new_pos = [line[-1][0] + steps[idx][0], line[-1][1] + steps[idx][1]] 
+      until !@board.on_chessboard?(new_pos)
+        line << new_pos
+      new_pos = [line[-1][0] + steps[idx][0], line[-1][1] + steps[idx][1]] 
+      end
+      lines += line
+    end
+    lines
+  end
 
   def move_diffs
     step_x = 1
@@ -32,5 +49,6 @@ module Slideable
   def horizontal_dirs
     directions = permutations.select.with_index {|e| e[1] != e[3]}
   end
+
 
 end
